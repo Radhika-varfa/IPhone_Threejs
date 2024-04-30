af4bc6b0e8a378cca272bd9f9f699229792c9b26
Radhika-varfa <rvarfa93@gmail.com>
2024-04-30 19:15:54 +0530

    Secound Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
 import Model from "./components/Model"
+import Features from "./components/Features"
+import * as Sentry from '@sentry/react';
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
+
+
 
 const App = () => {
-
   return (
     <>
     <main className="bg-black">
@@ -12,9 +17,12 @@ const App = () => {
    <Hero/>
    <Highlights/>
    <Model/>
+   <Features/>
+   <HowItWorks/>
+  <Footer/>
     </main>
     </>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://09408ae6be6e9034fe7f1260628cae96@o4507175089012736.ingest.us.sentry.io/4507175096090624",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect:React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
