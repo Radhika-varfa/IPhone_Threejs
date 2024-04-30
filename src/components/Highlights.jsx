5e91f96ecc590d1d4d8b1c41b3a30ac3cc2d1bd9
Radhika-varfa <rvarfa93@gmail.com>
2024-04-30 16:04:50 +0530

    first commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,35 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React from 'react'
+import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
+
+const Highlights = () => {
+  useGSAP(() => {
+    gsap.to('#title',{opacity:1, y:0})
+    gsap.to('.link',{opacity:1, y:0, duration:1, stagger:0.25})
+  },[])
+  return (
+<section id="hightlights" className='w-sceen overflow-hidden h-full common-padding bg-zinc'>
+  <div className='screen-max-width'>
+    <div className='md-12 w-full md:flex items-end '>
+      <h1 id="title" className='section-heading'>Get the hightlights.</h1>
+      <div className='flex flex-wrap items-end'>
+        <p className='link'>
+          Watch the film
+          <img src={watchImg} alt='watch' className='ml-2'/>
+        </p>
+        <p className='link'>
+          Watch the event
+          <img src={rightImg} alt='right' className='ml-2'/>
+        </p>
+      </div>
+    </div>
+<VideoCarousel/>
+  </div>
+
+</section>
+  )
+}
+
+export default Highlights
